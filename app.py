7fe5d917b6f4dda75703335606d58c2f721d2ee0
Thomas Bartlett <116368168+thom-bartlett@users.noreply.github.com>
2023-09-18 15:56:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,22 @@ from slack_bolt.adapter.flask import SlackRequestHandler
 
 app = App()
 
+@app.middleware  # or app.use(log_request)
+def log_request(logger, body, next):
+    logger.debug(body)
+    return next()
+
+
+@app.event("app_mention")
+def event_test(body, say, logger):
+    logger.info(body)
+    say("What's up?")
+
+
+@app.event("message")
+def handle_message():
+    pass
+
 flask_app = Flask(__name__)
 handler = SlackRequestHandler(app)
 
