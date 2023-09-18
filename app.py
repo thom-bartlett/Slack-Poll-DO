c60ca591336077c007efb977576a7e36b5ff9405
Thomas Bartlett <116368168+thom-bartlett@users.noreply.github.com>
2023-09-18 15:49:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
+from slack_bolt import App
+from slack_bolt.adapter.flask import SlackRequestHandler
 
-app = Flask(__name__)
+app = App()
 
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(request)
