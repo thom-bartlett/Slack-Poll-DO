5b21ba2ac066f251c3d918a00d15d78374bb4613
Thomas Bartlett <tbartlett@venturewell.org>
2023-09-25 14:35:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -181,10 +181,10 @@ def open_modal(ack, shortcut, client):
         view=creation_View
     )
 
-@app.error
-def custom_error_handler(error, body, logger):
-    logger.exception(f"Error: {error}")
-    logger.info(f"Request body: {body}")
+@app.middleware  # or app.use(log_request)
+def log_request(logger, body, next):
+    logger.debug(body)
+    return next()
     
 @app.action("visibility-action")
 def handle_visibility(ack, body, logger):
