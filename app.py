7367358e725a72832a1f77b5b58d290c56d81c30
Thomas Bartlett <tbartlett@venturewell.org>
2023-09-25 20:54:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -349,7 +349,6 @@ def handle_view_events(ack, body, logger, client):
         logger.info(f"Try result = {result}")
         return time
     except SlackApiError as e:
-        logger.info(f"except result = {result}")
         if e == "channel_not_found":
             logger.info("Bot not in channel")
             client.views_push(
